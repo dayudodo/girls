85af84c10c058339e8a53c10592ef4fbdc801164
dayudodo <dayudodo@gmail.com>
2015-10-14 22:46:17 +0800

    sinatra first study votes app

---
+++ views/layout.rb
@@ -0,0 +1,11 @@
+<!DOCTYPE html><html>  
+<head>
+	<meta charset='UTF-8' />
+	<title>Suffragist</title>   
+	<link href='//cdn.bootcss.com/bootstrap/2.3.1/css/bootstrap.min.css' rel='stylesheet' /> 
+</head> 
+<body class='container'>    
+	<h1><%= @title %></h1>    
+	<%= yield %> 
+</body>
+</html>
